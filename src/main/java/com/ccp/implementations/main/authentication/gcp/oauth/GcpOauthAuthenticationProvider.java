4570e077660f4d569b23fa0aad6c8f1641875dad
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-07-31 00:02:25 -0300

    AOP

--- src/main/java/com/ccp/implementations/main/authentication/gcp/oauth/GcpOauthAuthenticationProvider.java
+++ src/main/java/com/ccp/implementations/main/authentication/gcp/oauth/GcpOauthAuthenticationProvider.java
@@ -16,17 +16,13 @@ public class GcpOauthAuthenticationProvider implements CcpAuthenticationProvider
 
 	
 	public String getJwtToken() {
-		try {
-			CcpStringDecorator ccpStringDecorator = new CcpStringDecorator("GOOGLE_APPLICATION_CREDENTIALS");
-			CcpInputStreamDecorator inputStreamFrom = ccpStringDecorator.inputStreamFrom();
-			InputStream file = inputStreamFrom.fromEnvironmentVariablesOrClassLoaderOrFile();
-			GoogleCredential credential = GoogleCredential.fromStream(file)
-					.createScoped(Collections.singleton("https://www.googleapis.com/auth/cloud-platform"));
-			credential.refreshToken();
-			String accessToken = credential.getAccessToken();
-			return accessToken;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		CcpStringDecorator ccpStringDecorator = new CcpStringDecorator("GOOGLE_APPLICATION_CREDENTIALS");
+		CcpInputStreamDecorator inputStreamFrom = ccpStringDecorator.inputStreamFrom();
+		InputStream file = inputStreamFrom.fromEnvironmentVariablesOrClassLoaderOrFile();
+		GoogleCredential credential = GoogleCredential.fromStream(file)
+				.createScoped(Collections.singleton("https://www.googleapis.com/auth/cloud-platform"));
+		credential.refreshToken();
+		String accessToken = credential.getAccessToken();
+		return accessToken;
 	}
 }
